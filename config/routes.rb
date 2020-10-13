4932d42be0514cf768a373e579b8eae0fa4a01c2
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-10-13 10:18:22 +0100

    Add Gemfiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :show] 
   root to: "home#index"
